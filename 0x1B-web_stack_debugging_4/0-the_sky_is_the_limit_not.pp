31e892570685e09bd8c80f3177977fd448d3c0ea
menya96 <menyahtonny@gmail.com>
2024-08-23 06:16:48 -0700

    fixed nginx error with apachebench

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Add the number of files nginx can open
+exec { 'increase open file limit':
+    command => "sed -i 's/15/65536/g' /etc/default/nginx; sudo service nginx restart",
+    path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Still increasing file open numbers
+exec { 'increase hard file numbers':
+    provider => shell,
+    command  => "sudo sed -i 's/nofile 5/nofile 65536/' /etc/security/limits.conf",
+    before   => Exec['fileTwo']
+}
+exec { 'fileTwo':
+    provider => shell,
+    command  => "sudo sed -i 's/nofile 4/nofile 65536/' /etc/security/limits.conf",
+}
