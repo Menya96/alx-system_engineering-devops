2c9d488d5a1197d867c569b3a42c95e4e3335eb1
Menya96 <menyahtonny@gmail.com>
2024-09-26 07:17:11 -0700

    error fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get
+"""How many subs"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    """Returns number of subscribers for the passed in subreddit"""
+    apiURL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    resp = requests.get(
+        apiURL,
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if resp.status_code != 200:
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    jsonData = resp.json()
+    noOfSubs = jsonData.get('data').get('subscribers')
+    return noOfSubs
